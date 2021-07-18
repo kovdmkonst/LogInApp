694186b04a8785a97c232e1d8129beeee8de91e9
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2021-07-18 19:30:35 +1000

    finish task

--- LogInApp/LoginViewController.swift
+++ LogInApp/LoginViewController.swift
@@ -9,33 +9,55 @@ import UIKit
 
 class LoginViewController: UIViewController {
     
+    @IBOutlet weak var userNameTF: UITextField!
+    @IBOutlet weak var userPasswordTF: UITextField!
     
-    let user = "User"
-    let password = "Password"
+    private let user = "User"
+    private let password = "Password"
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        guard let welcomeVC = segue.destination as? WelcomeViewController else { return }
+        welcomeVC.user = user
     }
     
+    @IBAction func showForgottenName() {
+        showAlert(title: "Forgot your name?", message: "Your name is \(user)")
+    }
+    
+    @IBAction func showForgottenPassword() {
+        showAlert(title: "Forgot your password?", message: "Your password is \(password)")
+    }
+    
+    @IBAction func loginButtonPushed() {
+        if userNameTF.text != user || userPasswordTF.text != password {
+            showAlert(title: "Invalid login or password!",
+                      message: "Please, enter correct login and password.")
+            userPasswordTF.text = nil
+        } else {
+            performSegue(withIdentifier: "ShowWelcomeVC", sender: nil)
+        }
+    }
+    
+    @IBAction func unwind(segue: UIStoryboardSegue) {
+        userNameTF.text = ""
+        userPasswordTF.text = ""
+    }
 }
-
 
 
 // MARK: - Extensions
 
-//extension LoginViewController {
-//    private func showAlert(title: String,
-//                           message: String,
-//                           textField: UITextField) {
-//
-//        let alert = UIAlertController(title: title,
-//                                      message: message,
-//                                      preferredStyle: .alert)
-//
-//        let okeyButton = UIAlertAction(title: "OK",
-//                                       style: .default)
-//        alert.addAction(okeyButton)
-//        present(alert, animated: true)
-//        textField.text = nil
-//    }
-//}
+extension LoginViewController {
+    private func showAlert (title: String,
+                            message: String) {
+        
+        let alert = UIAlertController(title: title,
+                                      message: message,
+                                      preferredStyle: .alert)
+        
+        let okeyButton = UIAlertAction(title: "OK",
+                                       style: .default)
+        alert.addAction(okeyButton)
+        present(alert, animated: true)
+    }
+}
--- LogInApp/WelcomeViewController.swift
+++ LogInApp/WelcomeViewController.swift
@@ -9,21 +9,17 @@ import UIKit
 
 class WelcomeViewController: UIViewController {
 
+    var user = String()
+    
+    @IBOutlet weak var welcomeLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        welcomeLabel.text = """
+Welcome, \(user)!
+👋
+"""
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+    
 }
