57de4de77431a533e4239296143fd3fd05ce34cc
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2021-07-18 21:07:03 +1000

    add MVC components

---
+++ LogInApp/Model/Person.swift
@@ -0,0 +1,20 @@
+//
+//  Person.swift
+//  LogInApp
+//
+//  Created by Kovalyov Dmitry on 18.07.2021.
+//
+
+import Foundation
+
+struct Person {
+    
+    var name: String
+    var surname: String
+    var age: Int
+    
+    var hobbyDetails: [String]
+    
+    var job: String
+    
+}
---
+++ LogInApp/Model/User.swift
@@ -0,0 +1,19 @@
+//
+//  User.swift
+//  LogInApp
+//
+//  Created by Kovalyov Dmitry on 18.07.2021.
+//
+
+import Foundation
+
+struct User {
+    var userName: String
+    var userPassword: String
+    var person: Person
+}
+
+extension User {
+    
+}
+
