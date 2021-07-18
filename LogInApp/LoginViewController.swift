d5ef83c51578409273af85c464966fcf0cf648a6
Kovalyov Dmitriy <kovdmkonst@gmail.com>
2021-07-18 13:03:23 +1000

    add new ViewController

---
+++ LogInApp/LoginViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  LogInApp
+//
+//  Created by Kovalyov Dmitry on 14.07.2021.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+    
+    
+    let user = "User"
+    let password = "Password"
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
+
+
+
+// MARK: - Extensions
+
+//extension LoginViewController {
+//    private func showAlert(title: String,
+//                           message: String,
+//                           textField: UITextField) {
+//
+//        let alert = UIAlertController(title: title,
+//                                      message: message,
+//                                      preferredStyle: .alert)
+//
+//        let okeyButton = UIAlertAction(title: "OK",
+//                                       style: .default)
+//        alert.addAction(okeyButton)
+//        present(alert, animated: true)
+//        textField.text = nil
+//    }
+//}
